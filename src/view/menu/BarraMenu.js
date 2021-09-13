7e5f17bf9b203aeb29165a9484fe0100aa7c63e0
jessicapcruz <jessica_p_cruz@hotmail.com>
2021-09-12 23:44:39 -0300

    menu cadastro de conteudo

--- src/view/menu/BarraMenu.js
+++ src/view/menu/BarraMenu.js
@@ -117,6 +117,7 @@ export default function PrimarySearchAppBar() {
     >
       <MenuItem onClick={handleMenuClose}>Perfil</MenuItem>
       <MenuItem onClick={handleMenuClose}>Minha conta</MenuItem>
+      <MenuItem onClick={handleMenuClose}>Cadastrar conteúdo</MenuItem>
     </Menu>
   );
 
