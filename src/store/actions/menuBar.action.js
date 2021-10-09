15e43690ea03d8e0846ebbf4aa1e4b9e13778bac
jessicapcruz <jessica_p_cruz@hotmail.com>
2021-10-08 23:10:52 -0300

    remoção de console

--- src/store/actions/menuBar.action.js
+++ src/store/actions/menuBar.action.js
@@ -35,8 +35,6 @@ export const searchContent = (data) => {
             'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSsOpc3NpY2EiLCJzdWIiOiI2MTU1MTI1YmQzZWZjZDdiODM1NWNhYjciLCJlbWFpbCI6Implc3NpY2FfcF9jcnV6QGhvdG1haWwuY29tIiwiaWF0IjoxNjMzNjYwOTg5LCJleHAiOjE2MzM3NDczODl9.CMKgEddQZeZ7_jj_SV3XBNtb5ySThagvppqH0Ai4E3o'
         },
     }).then(response => {
-        
-        console.log(JSON.stringify(response));
         if (response.status === 200) {
             localStorage.setItem('search_list', response.data);  
             window.location.replace(rootUrl + 'busca');
